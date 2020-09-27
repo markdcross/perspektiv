1d58f3ac8d9e4946cec6ba882d92d5f50b431b3d
Bertodemus <roberto@robertorubet.com>
2020-09-26 23:19:28 -0400

    updated layout, base structure

---
+++ script.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  $(document).ready(function(){
+    $('.collapsible').collapsible();
+  });
+
+
+
+  $(document).ready(function(){
+    $('.modal').modal();
+  });
+
+
+
+
+
+  // $(".test").click(function () {initMap();});
+
+// function initMap() {
+//   var uluru = {lat: -25.363, lng: 131.044};
+//   var map = new google.maps.Map(document.getElementById('map'), {
+//     zoom: 4,
+//     center: uluru
+//   });
+//   var marker = new google.maps.Marker({
+//     position: uluru,
+//     map: map
+//   });
+// }
+
+});
