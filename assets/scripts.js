0398753159308719ac17dcba73eaace91e89aba5
MDC <markdcross@gmail.com>
2020-09-29 08:23:42 -0400

    Updated directory paths

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,3 +1,30 @@
+// Berto's initial JS
+
+document.addEventListener('DOMContentLoaded', function () {
+    $(document).ready(function () {
+        $('.collapsible').collapsible();
+    });
+
+    $(document).ready(function () {
+        $('.modal').modal();
+    });
+
+    // $(".test").click(function () {initMap();});
+
+    // function initMap() {
+    //   var uluru = {lat: -25.363, lng: 131.044};
+    //   var map = new google.maps.Map(document.getElementById('map'), {
+    //     zoom: 4,
+    //     center: uluru
+    //   });
+    //   var marker = new google.maps.Marker({
+    //     position: uluru,
+    //     map: map
+    //   });
+    // }
+});
+
+// Mark's JS start
 $(document).ready(function () {
     var muralData = murals;
     console.log(muralData);
