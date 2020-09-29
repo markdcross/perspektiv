56564e24419f284cb77ff03e282d5c77c32acc36
MDC <markdcross@gmail.com>
2020-09-29 14:41:18 -0400

    Debugging CORS error

--- assets/scripts.js
+++ assets/scripts.js
@@ -27,38 +27,47 @@ document.addEventListener('DOMContentLoaded', function () {
 // Mark's JS start
 $(document).ready(function () {
     var muralData = murals;
-    console.log(muralData);
     // Display map
     // User clicks on map
     // -- For each mural, see if name/address matches
     // -- -- If so:
     // -- Capture values for subsequent API calls
-    // for (var i=0; i < muralData.length; i++)
+
+    // ---------------------
+    // Pull values from murals.json for API calls
+    // ---------------------
+    // for (var i = 0; i < muralData.length; i++)
     // Capture and display mural address
     var address = muralData[1].address;
-    console.log('address:' + address);
     // Capture and display mural name
     var muralName = muralData[1].name;
-    console.log('name: ' + muralName);
-    // Capture and display 
+    // Capture and display
     var muralLoc = muralData[1].ExtendedData.Data[1].value;
-    console.log('loc: ' + muralLoc);
     // Capture and display
     var artistName = muralData[1].ExtendedData.Data[3].value;
-    console.log('artist: ' + artistName);
     $('#artist-info').text(`Artist: ${artistName}`);
     // Capture and display
     var artistWebsite = muralData[1].ExtendedData.Data[5].value;
-    console.log('website: ' + artistWebsite);
     // Capture and display
     var muralImg = muralData[1].ExtendedData.Data[6].value.__cdata;
-    console.log('Img: ' + muralImg);
     $('#mural-img').attr('src', muralImg);
-
     // -- -- Lat/Lon
     // -- -- Artist name
     // -- -- Artist website
     // Call Yelp API
+    var settings = {
+        url: `https://api.yelp.com/v3/businesses/search?location=${address}&radius=500`,
+        method: 'GET',
+        // timeout: 0,
+        headers: {
+            Authorization:
+                'Bearer VJmUSOlUKe1A9ZWkT-vaXD5r7SBOaEQij7d33Tjlcmw6yNPqInDhIVGoPXeLvMA8TSHWRGQEenRv0mKtq4CmxUKbWSOAh30oAtt71oAwLYg-xJNUulBSvIE6IXZzX3Yx',
+        },
+    };
+
+    $.ajax(settings).done(function (yelpResponse) {
+        console.log(yelpResponse);
+    });
     // -- Pass lat/lon through
     // -- Request nearby attractions based on filters
     // Call Wiki API
