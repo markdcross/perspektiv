bc1c094b668bf8e23f6534c2a120de6c8eb4e816
Bertodemus <roberto@robertorubet.com>
2020-10-02 07:23:35 -0400

    updates to interface

--- pushpin.js
+++ pushpin.js
@@ -35,7 +35,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.collapsible');
     var instances = M.Collapsible.init(elems);
 
-    // var elems = document.querySelectorAll('.materialboxed');
-    // var instances = M.Materialbox.init(elems);
+    var elems = document.querySelectorAll('.materialboxed');
+    var instances = M.Materialbox.init(elems);
 
 });
