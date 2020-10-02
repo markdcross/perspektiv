20fd14bb9a89ea923a7418c0a3d11b0326811fee
Bertodemus <roberto@robertorubet.com>
2020-10-01 21:26:19 -0400

    updates to UI and landing page

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,16 +1,19 @@
-const form = document.querySelector('#js-search-form');
-form.addEventListener('submit', handleSubmit);
+// const form = document.querySelector('#js-search-form');
+// form.addEventListener('submit', handleSubmit);
 
-function handleSubmit(event) {
-    // prevent page from reloading when form is submitted
-    event.preventDefault();
-    // get the value of the input field
-    const inputValue = document.querySelector('#js-search-input').value;
-    // remove whitespace from the input
-    const searchQuery = inputValue.trim();
-    // print `searchQuery` to the console
-    console.log(searchQuery);
-    searchWikipedia(searchQuery);
+// function handleSubmit(event) {
+//     // prevent page from reloading when form is submitted
+//     event.preventDefault();
+//     // get the value of the input field
+//     const inputValue = document.querySelector('#js-search-input').value;
+//     // remove whitespace from the input
+//     const searchQuery = inputValue.trim();
+//     // print `searchQuery` to the console
+//     console.log(searchQuery);
+//     searchWikipedia(searchQuery);
+
+
+
 /*
     try {
   
@@ -26,7 +29,7 @@ function handleSubmit(event) {
     
     }
     */
-}
+// }
 
 
 // searching wikipedia
